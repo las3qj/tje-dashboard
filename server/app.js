8a22be556162f53581ef17adb65997ab21918526
las3qj <las3qj@virginia.edu>
2021-05-31 17:00:05 -0400

    teacher test

--- server/app.js
+++ server/app.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const cors = require('cors');
-const app = express();
 const db = require('./firebase');
+const app = express();
 app.use(express.json());
 app.use(cors({origin: true}));
 const port = 8000;
 
+const testTeacherCollection = async () => {
+    const snapshot = await db.collection("teacher").get();
+    const teachers = [];
+    snapshot.forEach(doc => {
+        const teacher = {...doc.data(), id: doc.id};
+        console.log(teacher);
+        teachers.push(teacher);
+    });
+    return teachers;
+};
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
+})
+
+app.get('/teacherTest', (req, res) => {
+    testTeacherCollection().then(resp => res.json(resp));
 })
 
 app.listen(port, () => {
