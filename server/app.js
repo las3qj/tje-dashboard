48d6cea23d2d699f1f213b4f33110fbaf64bfc1a
las3qj <las3qj@virginia.edu>
2021-05-31 17:06:37 -0400

    getAll requests

--- server/app.js
+++ server/app.js
@@ -6,23 +6,64 @@ app.use(express.json());
 app.use(cors({origin: true}));
 const port = 8000;
 
-const testTeacherCollection = async () => {
+const getAllTeachers = async () => {
     const snapshot = await db.collection("teacher").get();
     const teachers = [];
     snapshot.forEach(doc => {
         const teacher = {...doc.data(), id: doc.id};
-        console.log(teacher);
         teachers.push(teacher);
     });
     return teachers;
-};
+}
+
+const getAllClasses = async () => {
+    const snapshot = await db.collection("class").get();
+    const classes = [];
+    snapshot.forEach(doc => {
+        const c = {...doc.data(), id: doc.id};
+        classes.push(c);
+    });
+    return classes;
+}
+
+const getAllStudents = async () => {
+    const snapshot = await db.collection("student").get();
+    const students = [];
+    snapshot.forEach(doc => {
+        const student = {...doc.data(), id: doc.id};
+        students.push(student);
+    });
+    return students;
+}
+
+const getAllEvents = async () => {
+    const snapshot = await db.collection("event").get();
+    const events = [];
+    snapshot.forEach(doc => {
+        const event = {...doc.data(), id: doc.id};
+        events.push(event);
+    });
+    return events;
+}
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 })
 
-app.get('/teacherTest', (req, res) => {
-    testTeacherCollection().then(resp => res.json(resp));
+app.get('/teachers', (req, res) => {
+    getAllTeachers().then(resp => res.json(resp));
+})
+
+app.get('/students', (req, res) => {
+    getAllStudents().then(resp => res.json(resp));
+})
+
+app.get('/classes', (req, res) => {
+    getAllClasses().then(resp => res.json(resp));
+})
+
+app.get('/events', (req, res) => {
+    getAllEvents().then(resp => res.json(resp));
 })
 
 app.listen(port, () => {
