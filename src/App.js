4ebc6db856ee8cc42ea00fb6c95f290b1aa0c74c
Ben Life <77246839+benlife5@users.noreply.github.com>
2021-06-01 08:58:39 -0400

    set up CalendarPage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-import HomePage from "./components/HomePage"
+import "./App.css";
+import HomePage from "./components/HomePage";
 import { Route, Switch } from "react-router-dom";
-import ErrorComponent from "./components/ErrorComponent"
+import ErrorComponent from "./components/ErrorComponent";
+import CalendarPage from "./components/CalendarPage";
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route path="/" component={HomePage} exact />
+        <Route path="/calendar" component={CalendarPage} />
         <Route component={ErrorComponent} />
       </Switch>
     </div>
---
+++ src/components/CalendarPage.js
@@ -0,0 +1,11 @@
+import NavBar from "./NavBar";
+
+function CalendarPage() {
+  return (
+    <div>
+      <NavBar /> <h1>Calendar</h1>{" "}
+    </div>
+  );
+}
+
+export default CalendarPage;
