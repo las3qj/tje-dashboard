723bc154fe7ad9b8ecd984286aee59422acae8f5
kslewis19 <60162532+kslewis19@users.noreply.github.com>
2021-06-02 11:41:38 -0400

    added contact us section

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,6 +2,8 @@ import NavBar from "./NavBar"
 import { makeStyles } from '@material-ui/core/styles';
 import background from './background.png'
 import Card from '@material-ui/core/Card';
+import Typography from '@material-ui/core/Typography';
+import CardContent from '@material-ui/core/CardContent';
 
 const useStyles = makeStyles((theme) => ({
     container: {
@@ -16,7 +18,12 @@ const useStyles = makeStyles((theme) => ({
     },
     contact: {
         flex: 1,
-        backgroundColor: "blue"
+        display: "flex",
+        flexDirection: "column",
+        backgroundColor: "lightgrey",
+        alignItems: "center",
+
+
     },
     image: {
         flex: 1.5,
@@ -30,7 +37,13 @@ const useStyles = makeStyles((theme) => ({
     mission: {
         flex: 1,
         backgroundColor: "purple"
+    },
+    card: {
+        width: "90%",
+        maxHeight: "30vh",
+        marginBottom: 20
     }
+
 
 }));
 
@@ -45,10 +58,47 @@ export default function HomePage() {
 
                     </div>
                     <div className={classes.mission}>
-
+                        mission statement/ about us
                     </div>
                 </div>
                 <div className={classes.contact}>
+                    <h1> Contact Us</h1>
+                    <Card className={classes.card}>
+                        <CardContent>
+                            <Typography className={classes.title} color="textSecondary" gutterBottom>
+                                Location
+                            </Typography>
+                            <Typography variant="body2" component="p">
+                                Thomas Jefferson Elementary School <br />
+                                1234 Thomas Jefferson Drive <br />
+                                Charlottesville, VA 22901 <br />
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                    <Card className={classes.card}>
+                        <CardContent>
+                            <Typography className={classes.title} color="textSecondary" gutterBottom>
+                                Principal
+                            </Typography>
+                            <Typography variant="body2" component="p">
+                                Camille Cooper <br />
+                                camille@joinforge.co <br />
+                                555-555-5555 <br />
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                    <Card className={classes.card}>
+                        <CardContent>
+                            <Typography className={classes.title} color="textSecondary" gutterBottom>
+                                Main Office
+                            </Typography>
+                            <Typography variant="body2" component="p">
+                                John Smith <br />
+                                john@joinforge.co <br />
+                                555-555-5555 <br />
+                            </Typography>
+                        </CardContent>
+                    </Card>
                 </div>
             </div>
         </div>
