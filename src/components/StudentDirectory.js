90b17e3cbb42fdebf42f365b729bbaf70d383961
Teeratham Vitchutripop <teerathamvitchutripop@Teerathams-MBP.fios-router.home>
2021-06-03 16:14:22 -0400

    UI adjustments

--- src/components/StudentDirectory.js
+++ src/components/StudentDirectory.js
@@ -128,7 +128,7 @@ export default function StudentDirectory() {
                 </div>
                 <div className={classes.studentList} style={{ margin: "auto" }}>
                      <div style={{paddingBottom:"0.5%"}}> 
-                      <Card elevation={2} style={{ width: "90vw", height: "4vw", margin:"auto",paddingBottom:"10px",backgroundColor:"#2E3B55"}}>
+                      <Card elevation={2} style={{ width: "90vw", height: "3vw", margin:"auto",paddingBottom:"10px",backgroundColor:"#2E3B55"}}>
                       <Grid container item xs={12} spacing={1} style={{alignItems: "center", height: "100%" }}>
                         <Grid item xs={2}>
                         <p style={{textAlign:"center",fontSize:"1.2vw",color:"white",fontWeight:"bold"}}>Last Name</p>
@@ -136,10 +136,13 @@ export default function StudentDirectory() {
                         <Grid item xs={2}>
                         <p style={{textAlign:"center",fontSize:"1.2vw",color:"white",fontWeight:"bold"}}>First Name</p>
                         </Grid>
-                        <Grid item xs={4}>
+                        <Grid item xs={1.5}>
                         <p style={{textAlign:"center",fontSize:"1.2vw",color:"white",fontWeight:"bold"}}>Contact Information</p>
                         </Grid>
-                        <Grid item xs={4}>
+                        <Grid item xs={1}>
+                        <p style={{textAlign:"center",fontSize:"1.2vw",color:"white",fontWeight:"bold"}}>D.O.B</p>
+                        </Grid>
+                        <Grid item xs={3}>
                           <p style={{textAlign:"center",fontSize:"1.2vw",color:"white",fontWeight:"bold"}}>Classes</p>
                         </Grid>
                       </Grid>
--- src/components/TeacherDirectory.js
+++ src/components/TeacherDirectory.js
@@ -101,23 +101,26 @@ function TeacherDirectory() {
             </div>
             <Grid container spacing={1} style={{ justifyContent: "center" }}>
                 <div style={{paddingBottom:"0.5%"}}> 
-                  <Card elevation={2} style={{ width: "90vw", height: "4vw", margin:"auto",paddingBottom:"10px",backgroundColor:"#2E3B55"}}>
-                  <Grid container item xs={12} spacing={1} style={{alignItems: "center", height: "100%" }}>
-                    <Grid item xs={2}>
-                    <p style={{textAlign:"center",fontSize:"1.2vw",color:"white",fontWeight:"bold"}}>Last Name</p>
-                    </Grid>
-                    <Grid item xs={2}>
-                    <p style={{textAlign:"center",fontSize:"1.2vw",color:"white",fontWeight:"bold"}}>First Name</p>
-                    </Grid>
-                    <Grid item xs={4}>
-                    <p style={{textAlign:"center",fontSize:"1.2vw",color:"white",fontWeight:"bold"}}>Contact Information</p>
-                    </Grid>
-                    <Grid item xs={4}>
-                      <p style={{textAlign:"center",fontSize:"1.2vw",color:"white",fontWeight:"bold"}}>Classes</p>
-                    </Grid>
-                  </Grid>
-                </Card>
-                </div>
+                      <Card elevation={2} style={{ width: "90vw", height: "3vw", margin:"auto",paddingBottom:"10px",backgroundColor:"#2E3B55"}}>
+                      <Grid container item xs={12} spacing={1} style={{alignItems: "center", height: "100%" }}>
+                        <Grid item xs={2}>
+                        <p style={{textAlign:"center",fontSize:"1.2vw",color:"white",fontWeight:"bold"}}>Last Name</p>
+                        </Grid>
+                        <Grid item xs={2}>
+                        <p style={{textAlign:"center",fontSize:"1.2vw",color:"white",fontWeight:"bold"}}>First Name</p>
+                        </Grid>
+                        <Grid item xs={1.5}>
+                        <p style={{textAlign:"center",fontSize:"1.2vw",color:"white",fontWeight:"bold"}}>Contact Information</p>
+                        </Grid>
+                        <Grid item xs={1}>
+                        <p style={{textAlign:"center",fontSize:"1.2vw",color:"white",fontWeight:"bold"}}>D.O.B</p>
+                        </Grid>
+                        <Grid item xs={3}>
+                          <p style={{textAlign:"center",fontSize:"1.2vw",color:"white",fontWeight:"bold"}}>Classes</p>
+                        </Grid>
+                      </Grid>
+                    </Card>
+                </div >
                 {teachers !== "loading" && (teachersToDisplay.map((teacher) => (
                     <PersonCard
                         personType={"teacher"}
