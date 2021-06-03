8252f9a34c2617646bfd12e1752d48363bf4fdfd
Ben Life <77246839+benlife5@users.noreply.github.com>
2021-06-03 15:26:45 -0400

    added color theme to calendar page

--- src/components/CalendarPage.js
+++ src/components/CalendarPage.js
@@ -7,7 +7,21 @@ import { getCalendarEvents, convertDate } from "../utils/CalendarUtils";
 import CalendarView from "./CalendarView";
 import { UserContext } from "../contexts/UserContext";
 import { useContext } from "react";
-import Footer from "./Footer"
+import Footer from "./Footer";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#2E3B55",
+      contrastText: "white",
+    },
+    secondary: {
+      main: "#FDFD96",
+      contrastText: "#2E3B55",
+    },
+  },
+});
 
 function CalendarPage() {
   const [events, setEvents] = useState();
@@ -26,85 +40,87 @@ function CalendarPage() {
   return (
     <div>
       <NavBar />
+      <ThemeProvider theme={theme}>
+        <h1>Calendar</h1>
 
-      <h1>Calendar</h1>
+        <AddCalendarEventDialog
+          open={addEventActive}
+          setOpen={setAddEventActive}
+          setEvents={setEvents}
+        />
 
-      <AddCalendarEventDialog
-        open={addEventActive}
-        setOpen={setAddEventActive}
-        setEvents={setEvents}
-      />
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "1fr 1fr 1fr",
+            width: "50%",
+            gap: "2%",
+            margin: "auto",
+          }}
+        >
+          {view === "list" && (
+            <Button
+              variant="outlined"
+              color="primary"
+              onClick={() => setView("calendar")}
+            >
+              View as Calendar
+            </Button>
+          )}
 
-      <div
-        style={{
-          display: "grid",
-          gridTemplateColumns: "1fr 1fr 1fr",
-          width: "50%",
-          gap: "2%",
-          margin: "auto",
-        }}
-      >
-        {view === "list" && (
-          <Button
-            variant="outlined"
-            color="primary"
-            onClick={() => setView("calendar")}
-          >
-            View as Calendar
-          </Button>
-        )}
+          {view === "calendar" && (
+            <Button
+              variant="outlined"
+              color="primary"
+              onClick={() => setView("list")}
+            >
+              View as List
+            </Button>
+          )}
 
-        {view === "calendar" && (
-          <Button
-            variant="outlined"
-            color="primary"
-            onClick={() => setView("list")}
-          >
-            View as List
-          </Button>
-        )}
+          {isLoggedIn ? (
+            <Button
+              color="secondary"
+              variant="contained"
+              // style={{backgroundColor:"#FDFD96"}}
+              onClick={() => setAddEventActive(true)}
+            >
+              Add Event
+            </Button>
+          ) : (
+            <div></div>
+          )}
 
-        {isLoggedIn ? (
-          <Button
-            color="primary"
-            variant="contained"
-            onClick={() => setAddEventActive(true)}
-          >
-            Add Event
-          </Button>
-        ) : (
-          <div></div>
-        )}
+          {view === "list" && (
+            <Button
+              variant="outlined"
+              color="primary"
+              onClick={() => setShowPastEvents((show) => !show)}
+            >
+              {showPastEvents ? "Hide Past Events" : "Show Past Events"}
+            </Button>
+          )}
+        </div>
 
-        {view === "list" && (
-          <Button
-            variant="outlined"
-            color="primary"
-            onClick={() => setShowPastEvents((show) => !show)}
-          >
-            {showPastEvents ? "Hide Past Events" : "Show Past Events"}
-          </Button>
-        )}
-      </div>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            flexDirection: "column",
+            marginTop: "2%",
+            gap: "1%",
+          }}
+        >
+          {view === "list" &&
+            events &&
+            displayedEvents.map((item) => (
+              <CalendarCard event={item} key={item.id} setEvents={setEvents} />
+            ))}
 
-      <div
-        style={{
-          display: "flex",
-          alignItems: "center",
-          flexDirection: "column",
-          marginTop: "2%",
-          gap: "1%",
-        }}
-      >
-        {view === "list" &&
-          events &&
-          displayedEvents.map((item) => (
-            <CalendarCard event={item} key={item.id} setEvents={setEvents} />
-          ))}
-
-        {view === "calendar" && events && <CalendarView events={events} />}
-      </div>
-      <Footer/>
+          {view === "calendar" && events && <CalendarView events={events} />}
+        </div>
+      </ThemeProvider>
+      <Footer />
     </div>
   );
 }
