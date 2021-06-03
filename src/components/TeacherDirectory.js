e09bbe85671d43f63ca4dc61fbf2f2c15b9ae23a
Ben Life <77246839+benlife5@users.noreply.github.com>
2021-06-03 10:51:49 -0400

    added view for access codes

--- src/components/TeacherDirectory.js
+++ src/components/TeacherDirectory.js
@@ -17,6 +17,7 @@ function TeacherDirectory(){
     const [search, setSearch] = useState("")
     const [sort,setSort]=useState(false);
     const [classList, setClassList] = useState([]);
+    console.log(teachers)
 
     const getTeachers=(()=>{
         fetch("http://localhost:8000/classes")
@@ -106,6 +107,7 @@ function TeacherDirectory(){
                   classList={classList}
                   sort={sort}
                   setSort={setSort}
+                  accessCode={teacher.id}
                 />
               )))}
             {teachersToDisplay.length === 0 && teachers !== "loading" && ("No results found")}
