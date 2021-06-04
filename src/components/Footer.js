4b170f94858c4d2a756709d541679d4bbaa8af9e
Teeratham Vitchutripop <teerathamvitchutripop@Teerathams-MBP.fios-router.home>
2021-06-04 09:57:39 -0400

    Few tweaks to Home and Footer UI

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,7 +16,7 @@ function Footer(){
                 <div style={{paddingLeft:40}}>
                 <h1 style={{marginBottom:"-10px"}}>🌎 📖 🏆</h1>
                 <h4 style={{color:"#FDFD96",marginBottom:"-3px",fontSize:"1.2vw"}}>Positivity. Respect. Diversity. Education. Comradery.</h4>
-                <p style={{color:"white",fontSize:"1vw",textAlign:"justify"}}>At Thomas Jefferson Elementary School, we are a diverse community of global citizens and lifelong learners that lead by example. We believe in the power of positivity, respect, and cooperation. We manifest our potential through confidence and work ethic.</p>
+                <p style={{color:"white",fontSize:"1vw",textAlign:"justify", fontWeight:"200"}}>At Thomas Jefferson Elementary School, we are a diverse community of global citizens and lifelong learners that lead by example. We believe in the power of positivity, respect, and cooperation. We manifest our potential through confidence and work ethic.</p>
                 </div>
             </Grid>
             <Grid item xs={4}>
